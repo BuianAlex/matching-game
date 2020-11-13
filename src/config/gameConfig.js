64b56b7f1222bb6f30c6e009c748bb20eba78242
BuianAlex <a.buyan@ukr.net>
2020-11-13 23:28:26 +0200

    add config file

---
+++ src/config/gameConfig.js
@@ -0,0 +1,8 @@
+export const sizeButtons = [
+  { name: '4x3', size: 12 },
+  { name: '4x4', size: 16 },
+  { name: '5x4', size: 20 },
+  { name: '6x5', size: 30 },
+  { name: '6x6', size: 36 },
+  { name: '7x6', size: 42 },
+];
